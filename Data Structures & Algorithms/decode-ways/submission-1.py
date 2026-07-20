244c145921f911e6724a92602c368db5b19ae7f3
Laksha <lakshasangtani.26@gmail.com>
2026-07-20 14:37:49 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        memo = {len(s) : 1} # 1 valid string for s
+
+        def check(i):
+            if i in memo:
+                return memo[i]
+            if s[i] == "0":
+                return 0
+            
+            #check 1 digit
+            res = check(i+1)
+
+            if i+1 < len(s) and (s[i] == '1' or (s[i]=='2' and s[i+1] in "0123456")):
+                res += check(i+2)
+
+            memo[i] = res
+            return res
+        
+        return check(0)
+        
