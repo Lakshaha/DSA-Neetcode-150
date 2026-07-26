6c5fb4ced8b3dca74e774048ef367d7275d4dd55
Laksha <lakshasangtani.26@gmail.com>
2026-07-27 01:03:22 +0530

    Add: design-hashset - submission-0

---
+++ Data Structures & Algorithms/design-hashset/submission-0.py
@@ -0,0 +1,31 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.cap = 10007
+        self.hmap = [[] for _ in range(self.cap)]
+        
+    def _hash(self, x:int):
+        return x % (self.cap)
+
+    def add(self, key: int) -> None:
+        idx = self._hash(key)
+        if key not in self.hmap[idx]:
+            self.hmap[idx].append(key)
+
+    def remove(self, key: int) -> None:
+        idx = self._hash(key)
+        if key in self.hmap[idx]:
+            self.hmap[idx].remove(key)
+        
+
+    def contains(self, key: int) -> bool:
+        idx = self._hash(key)
+        return key in self.hmap[idx]
+        
+
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
